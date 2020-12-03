f8bc7a698d408cb766cc65b9afd1f5a37858473c
Sniper89-hash <73473891+Sniper89-hash@users.noreply.github.com>
2020-12-03 22:25:10 +0200

    Update Script.py

--- Script.py
+++ Script.py
@@ -6,32 +6,27 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.firefox.options import Options
 
-options = Options(); options.headless = False
-chrome_browser = webdriver.Chrome(options=options, executable_path=r'path\to\driver')
-chrome_browser.maximize_window()
+# phantom mode
+options = Options(); options.headless = True
+chrome_browser = webdriver.Firefox(options=options, executable_path=r'path\to\driver')
 
+# waiting for element tp be found
 def wait_element(delay, xpath): WebDriverWait(chrome_browser, delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
-def clear(): os.system('cls')
-def message(text): clear(); print(text); time.sleep(1)
-def search_another(mes_text):
-    while a == 1:
-        clear(); again = input(mes_text + '\n')
-        if again == 'y':
-            clear(); req = input('Enter what to search:\n')
-            if req == '': message('Input is empty!')
-            else: return req
-        elif again == 'n': message('Have a nice day!'); clear(); chrome_browser.quit(); exit()
-        else: message('Wrong input!')
 
-a = 1; request = 'iPhone 7'
-while a == 1:
-    try:
-        clear(); print('Processing...')
-        chrome_browser.get('https://hotline.ua/')
-        wait_element(15, "//div//input[@id='doSearch']")
-        chrome_browser.find_element_by_xpath("//input[@id='searchbox'][@data-default='... найти товар']").send_keys(request)
-        chrome_browser.find_element_by_xpath("//div//input[@id='doSearch']").click()
-        wait_element(15, "//h1[contains(text(), 'По запросу')]")
-        chrome_browser.find_element_by_xpath("//p[@class='h4']//a[@data-tracking-id='catalog-10']").click()
-        request = search_another('Product found!\n\nWant to search one more?')
-    except TimeoutException: request = search_another('Nothing found! Try again?')
+# clear terminal
+def clear(): os.system('cls')
+
+try:
+    start = time.time()
+    clear(); print('Processing... Please wait...')
+    chrome_browser.get('https://hotline.ua/')
+    wait_element(15, "//div//input[@id='doSearch']")
+    chrome_browser.find_element_by_xpath("//input[@id='searchbox'][@data-default='... найти товар']").send_keys('iPhone 7')
+    chrome_browser.find_element_by_xpath("//div//input[@id='doSearch']").click()
+    wait_element(15, "//h1[contains(text(), 'По запросу')]")
+    chrome_browser.find_element_by_xpath("//p[@class='h4']//a[@data-tracking-id='catalog-10']").click()
+    finish = time.time()
+    clear(); print(f'Finished successfully!\n\nTakes {finish-start}s')
+    chrome_browser.quit()
+except TimeoutException:
+    clear(); print('Nothing found!\n\nTry again!')
